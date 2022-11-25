5456eb6deca3684fb755f44320376b71042667ae
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2022-11-25 14:35:24 -0300

    initproyect

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:card_database_aplication/Pages/Home_Page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', home: HomePage());
+  }
+}
